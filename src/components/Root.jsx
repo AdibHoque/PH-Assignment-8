069ce5acdaf9694729f5859c11c6c0c03e4f5e0c
AdibHoque <adibhoque04@gmail.com>
2024-03-25 19:44:50 +0600

    Added NavBar

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,9 +1,13 @@
 import {Outlet} from "react-router-dom";
+import NavBar from "./Navbar";
 
 export default function Root() {
   return (
     <>
-      <Outlet></Outlet>
+      <div className="containter mx-auto px-2 lg:px-24">
+        <NavBar></NavBar>
+        <Outlet></Outlet>
+      </div>
     </>
   );
 }
