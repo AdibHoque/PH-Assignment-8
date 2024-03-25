e89bfa7c74848326b5025f1134bdb91f85a29fe1
AdibHoque <adibhoque04@gmail.com>
2024-03-25 19:06:54 +0600

    Initial React app  Setup with Tailwind & DaisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import DaisyUI from 'daisyui'
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [DaisyUI],
+}
+
